c324bcb961139ab9b3964aaca026396e0af7927f
icecrimu <charles.banigon@gmail.com>
2022-02-23 12:27:08 +0800

    user profile top

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,14 +1,85 @@
 import React, { useContext, useEffect } from "react"
-import { GithubContext } from "../context/github/GithubContext"
 import { useParams } from "react-router-dom"
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa"
+import { Link } from "react-router-dom"
+import Spinner from "../components/layout/Spinner"
+import { GithubContext } from "../context/github/GithubContext"
 
 export default function User() {
-  const { fetchUser, user } = useContext(GithubContext)
+  const { fetchUser, user, isLoading } = useContext(GithubContext)
+
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable
+  } = user
 
   const params = useParams()
 
   useEffect(() => {
     fetchUser(params.login)
   }, [])
-  return <div>User</div>
+
+  if (isLoading) {
+    return <Spinner />
+  }
+  return (
+    <>
+      <div className="w-full mx-auto lg:w-10/12">
+        <div className="mb-4">
+          <Link to="/" className="btn btn-ghost">
+            Back To Search
+          </Link>
+        </div>
+
+        <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
+          <div className="custom-card-image mb-6 md:mb-0">
+            <div className="rounded-lg shadow-xl card image-full">
+              <figure>
+                <img src={avatar_url} alt="avatar" />
+              </figure>
+              <div className="card-body justify-end">
+                <h2 className="card-title  mb-0">{name}</h2>
+                <p>{login}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-2">
+            <div className="mb-6">
+              <h1 className="text-3xl card-title">
+                {name}
+                <div className="ml-2 mr-1 badge badge-success">{type}</div>
+                {hireable && (
+                  <div className="mx-1 badge badge-info">Hireable</div>
+                )}
+              </h1>
+              <p>{bio}</p>
+              <div className="mt-4 card-actions">
+                <a
+                  href={html_url}
+                  target="_black"
+                  rel="noreferrer"
+                  className="btn btn-outline"
+                >
+                  Visit Github Profile
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
